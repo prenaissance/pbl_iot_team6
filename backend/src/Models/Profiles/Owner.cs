a09091f633ff69d1145d566052282c21770733ef
prenaissance <alexandru.andries@isa.utm.md>
2024-03-26 21:15:21 +0200

    feat(backend): bind device id to owner

--- backend/src/Models/Profiles/Owner.cs
+++ backend/src/Models/Profiles/Owner.cs
@@ -3,6 +3,7 @@ namespace Dispenser.Models.Profiles;
 public class Owner
 {
     public int Id { get; set; }
+    public Guid DeviceId { get; set; }
     public required string Username { get; set; }
     public required string PasswordHash { get; set; }
     public required string PasswordSalt { get; set; }
