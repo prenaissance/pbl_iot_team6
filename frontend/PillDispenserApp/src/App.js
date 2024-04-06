d8b19af4d596ff34f1eb586ccdb14b5d664e8bd3
micha <michael.echim@gmail.com>
2024-04-06 14:52:31 +0300

    frontend started ig

---
+++ frontend/PillDispenserApp/src/App.js
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Button,
+} from 'react-native';
+import { Colors, DebugInstructions, Header, LearnMoreLinks, ReloadInstructions } from "react-native/Libraries/NewAppScreen"
+import LogIn from './LogIn';
+
+function App (){
+  const isDarkMode = useColorScheme() === "dark"
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter
+  }
+
+  return(
+    <View>
+      <Button title='Led One On' />
+      <Button title='Led Two On' />
+      <Button title='Led Three On' />
+    </View>
+    // <LogIn/>
+  )
+}
+export default App
