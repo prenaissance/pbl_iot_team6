10ec0ca00128ca1c84611a30d32ddcadf5fc08b6
MaxCojocari <cojocarigoncearmax@gmail.com>
2024-04-15 23:22:32 +0300

    refactor: adjust for correct uid display

--- device-soft/ESP32/src/drivers/rfidDriver.h
+++ device-soft/ESP32/src/drivers/rfidDriver.h
@@ -1,84 +1,65 @@
-#include <SPI.h>
 #include <MFRC522.h>
+#include <SPI.h>
 
 #define MAX_KEY_SIZE 6
 
-class RfidDriver
-{
-private:
+class RfidDriver {
+   private:
     MFRC522 rfid;
     MFRC522::MIFARE_Key key;
 
-    String toStringHex(byte *buffer, byte bufferSize)
-    {
-        char charArray[bufferSize + 1];
+   public:
+    RfidDriver() : rfid(0, 0) {}
 
-        for (int i = 0; i < bufferSize; i++)
-        {
-            charArray[i] = (char)buffer[i];
-        }
+    RfidDriver(int ssPin, int rstPin) : rfid(ssPin, rstPin) {
+        SPI.begin();      // Init SPI bus
+        rfid.PCD_Init();  // Init MFRC522
 
-        charArray[bufferSize] = '\0';
-
-        return String(charArray);
-    }
-
-public:
-    RfidDriver() : rfid(0, 0)
-    {
-    }
-
-    RfidDriver(int ssPin, int rstPin) : rfid(ssPin, rstPin)
-    {
-        SPI.begin();     // Init SPI bus
-        rfid.PCD_Init(); // Init MFRC522
-
-        for (byte i = 0; i < MAX_KEY_SIZE; ++i)
-        {
+        for (byte i = 0; i < MAX_KEY_SIZE; ++i) {
             key.keyByte[i] = 0xFF;
         }
     }
 
-    String getUid()
-    {
-        return toStringHex(rfid.uid.uidByte, rfid.uid.size);
-    }
-
-    MFRC522 getRfid()
-    {
-        return rfid;
-    }
+    MFRC522 getRfid() { return rfid; }
 };
 
-class RfidDriverManager
-{
-private:
+class RfidDriverManager {
+   private:
     RfidDriver *instance;
 
-public:
-    void init(RfidDriver *driverInstance)
-    {
-        instance = driverInstance;
+    String convertHexToString(byte *buffer, byte bufferSize) {
+        String output = "";
+
+        for (byte i = 0; i < bufferSize; ++i) {
+            char hexBuff[3];
+            sprintf(hexBuff, "%02X", buffer[i]);
+            output += hexBuff;
+        }
+
+        return output;
     }
 
-    void manageUid()
-    {
-        // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
-        if (!instance->getRfid().PICC_IsNewCardPresent())
-            return;
+   public:
+    void init(RfidDriver *driverInstance) { instance = driverInstance; }
+
+    void manageUid() {
+        MFRC522 rfid = instance->getRfid();
+
+        // Reset the loop if no new card present on the sensor/reader. This
+        // saves the entire process when idle.
+        if (!rfid.PICC_IsNewCardPresent()) return;
 
         // Verify if the NUID has been readed
-        if (!instance->getRfid().PICC_ReadCardSerial())
-            return;
+        if (!rfid.PICC_ReadCardSerial()) return;
 
         // Record UID
-        String uidHex = instance->getUid();
+        String uidHex = convertHexToString(rfid.uid.uidByte, rfid.uid.size);
         Serial.println(uidHex);
 
         // Halt PICC
-        instance->getRfid().PICC_HaltA();
+        rfid.PICC_HaltA();
 
         // Stop encryption on PCD
-        instance->getRfid().PCD_StopCrypto1();
+        rfid.PCD_StopCrypto1();
     }
 };
--- device-soft/ESP32/src/main.cpp
+++ device-soft/ESP32/src/main.cpp
@@ -1,27 +1,33 @@
 #include <Arduino.h>
-
+#include <BLE2902.h>
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
-#include <BLE2902.h>
+#include <drivers/rfidDriver.h>
 
 // Use www.uuidgenerator.net to get new UUIDs
 
-#define BAUD_RATE 9600
+#define BAUD_RATE 115200
 
-#define SERVICE_UUID            "975b7600-ced6-4b3a-a4af-be038d43b8c9"
+#define SERVICE_UUID "975b7600-ced6-4b3a-a4af-be038d43b8c9"
 
-#define FETCH_SIGNAL_CHAR_UUID  "f62206df-79d6-4eb9-bfc8-72036bf1e3b3"
-#define FETCH_SIGNAL_CHAR_DESC  "Client sets this bool 'flag' to call local configuration fetching"
+#define FETCH_SIGNAL_CHAR_UUID "f62206df-79d6-4eb9-bfc8-72036bf1e3b3"
+#define FETCH_SIGNAL_CHAR_DESC "Client sets this bool 'flag' to call local configuration fetching"
 
-#define RFID_REQUEST_CHAR_UUID  "30e27b63-3ee2-486a-a14f-e020be483ada"
-#define RFID_REQUEST_CHAR_DESC  "Client uses this characteristic to request and recieve the RFID tag data"
+#define RFID_REQUEST_CHAR_UUID "30e27b63-3ee2-486a-a14f-e020be483ada"
+#define RFID_REQUEST_CHAR_DESC "Client uses this characteristic to request and recieve the RFID tag data"
 
-#define USER_ID_CHAR_UUID       "a4bd67ef-d298-460e-88fd-2857885b0724"
-#define USER_ID_CHAR_DESC       "ID to get from client when connecting"
+#define USER_ID_CHAR_UUID "a4bd67ef-d298-460e-88fd-2857885b0724"
+#define USER_ID_CHAR_DESC "ID to get from client when connecting"
 
-#define DEVICE_ID_CHAR_UUID     "5a9bec66-0f89-4383-b779-c3b9162d2814"
-#define DEVICE_ID_CHAR_DESC     "ID to send to client when connecting"
+#define DEVICE_ID_CHAR_UUID "5a9bec66-0f89-4383-b779-c3b9162d2814"
+#define DEVICE_ID_CHAR_DESC "ID to send to client when connecting"
+
+#define SS_PIN 5
+#define RST_PIN 0
+
+RfidDriver rfidDriver(SS_PIN, RST_PIN);
+RfidDriverManager rm;
 
 BLEServer *pServer = NULL;
 BLEService *pService = NULL;
@@ -48,7 +54,7 @@ const std::string deviceId = "1111-2222-3333";
 static std::string userId = "";
 
 class MyServerCallbacks : public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
+    void onConnect(BLEServer *pServer) {
         connectedClient = true;
         Serial.println("A client has connected!");
 
@@ -56,7 +62,7 @@ class MyServerCallbacks : public BLEServerCallbacks {
         pDeviceIDChar->notify();
     }
 
-    void onDisconnect(BLEServer* pServer) {
+    void onDisconnect(BLEServer *pServer) {
         connectedClient = false;
         Serial.println("The client has disconnected!");
 
@@ -76,14 +82,14 @@ class CharacteristicsCallbacks : public BLECharacteristicCallbacks {
     }
 };
 
-void setup(){
+void setup() {
     Serial.begin(BAUD_RATE);
 
     BLEDevice::init("ESP32");
 
-    /* 
-    The smartphone app initiates the connection, 
-    so the board is considered to be a server waiting 
+    /*
+    The smartphone app initiates the connection,
+    so the board is considered to be a server waiting
     for incoming connections
     */
 
@@ -100,25 +106,21 @@ void setup(){
 
     pFetchSignChar = pService->createCharacteristic(
         FETCH_SIGNAL_CHAR_UUID,
-        BLECharacteristic::PROPERTY_WRITE
-    );
+        BLECharacteristic::PROPERTY_WRITE);
 
     pRFIDReqChar = pService->createCharacteristic(
         RFID_REQUEST_CHAR_UUID,
-        BLECharacteristic::PROPERTY_WRITE   |
-        BLECharacteristic::PROPERTY_NOTIFY
-    );
+        BLECharacteristic::PROPERTY_WRITE |
+            BLECharacteristic::PROPERTY_NOTIFY);
 
     pUserIDChar = pService->createCharacteristic(
         USER_ID_CHAR_UUID,
-        BLECharacteristic::PROPERTY_WRITE
-    );
+        BLECharacteristic::PROPERTY_WRITE);
 
     pDeviceIDChar = pService->createCharacteristic(
         DEVICE_ID_CHAR_UUID,
         BLECharacteristic::PROPERTY_READ |
-        BLECharacteristic::PROPERTY_NOTIFY
-    );
+            BLECharacteristic::PROPERTY_NOTIFY);
 
     // Ass dome info descriptors for all the characteristics
 
@@ -139,7 +141,7 @@ void setup(){
     pDeviceIDChar->addDescriptor(pDeviceIDDesc);
 
     /*
-    For "notifications" to work properly it is necessary to 
+    For "notifications" to work properly it is necessary to
     configure them with the BLE2902 Descriptor
     */
 
@@ -157,7 +159,10 @@ void setup(){
     pFetchSignChar->setCallbacks(new CharacteristicsCallbacks());
     pRFIDReqChar->setCallbacks(new CharacteristicsCallbacks());
     pUserIDChar->setCallbacks(new CharacteristicsCallbacks());
+
+    rm.init(&rfidDriver);
 }
 
-void loop(){
+void loop() {
+    rm.manageUid();
 }
