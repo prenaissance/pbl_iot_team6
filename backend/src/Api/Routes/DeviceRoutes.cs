a6733187b540ccf5cf014c2724dc5056e9254ab7
prenaissance <alexandru.andries@isa.utm.md>
2024-05-26 23:59:00 +0300

    feat(backend): add event posting using get request

--- backend/src/Api/Routes/DeviceRoutes.cs
+++ backend/src/Api/Routes/DeviceRoutes.cs
@@ -5,6 +5,7 @@ using Dispenser.Dtos.Device.Events;
 using Dispenser.Dtos.PillSchedules;
 using Dispenser.Models.Audit;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Models;
@@ -13,6 +14,30 @@ namespace Dispenser.Api.Routes;
 
 public static class DeviceRoutes
 {
+    private static readonly Func<OpenApiOperation, OpenApiOperation> eventOpenapi =
+        config =>
+        {
+            var existingDeviceId = config.Parameters.FirstOrDefault(p => p.Name == "deviceId");
+            if (existingDeviceId is not null)
+            {
+                config.Parameters.Remove(existingDeviceId);
+            }
+            config.Parameters.Add(
+                new OpenApiParameter
+                {
+                    Name = "deviceId",
+                    In = ParameterLocation.Path,
+                    Required = true,
+                    Schema = new OpenApiSchema
+                    {
+                        Type = "string",
+                        Format = "uuid"
+                    },
+                    Example = new OpenApiString("11111111-1111-1111-1111-111111111111")
+                }
+            );
+            return config;
+        };
     public static IEndpointRouteBuilder RegisterDeviceRoutes(this IEndpointRouteBuilder routes)
     {
         var group = routes.MapGroup("/devices");
@@ -44,26 +69,11 @@ public static class DeviceRoutes
         group.MapPost("/{deviceId:guid}/event", AddEvent)
             .WithValidation<AddEventRequest>()
             .WithDescription("Add an event to the device audit log")
-            .WithOpenApi(
-                config =>
-                {
-                    config.Parameters = [
-                        new OpenApiParameter
-                        {
-                            Name = "deviceId",
-                            In = ParameterLocation.Path,
-                            Required = true,
-                            Schema = new OpenApiSchema
-                            {
-                                Type = "string",
-                                Format = "uuid"
-                            },
-                            Example = new OpenApiString("11111111-1111-1111-1111-111111111111")
-                        }
-                    ];
-                    return config;
-                }
-            );
+            .WithOpenApi(eventOpenapi);
+
+        group.MapGet("/{deviceId:guid}/event", AddEventWithGet)
+            .WithDescription("Add an event to the device audit log with GET (workaround)")
+            .WithOpenApi(eventOpenapi);
 
         return routes;
     }
@@ -121,6 +131,27 @@ public static class DeviceRoutes
             EventType = request.Body.EventType,
             EventData = request.Body.EventData,
             ProfileId = request.Body.ProfileId
+        });
+
+        await db.SaveChangesAsync();
+
+        return TypedResults.Created();
+    }
+
+    private static async Task<Created> AddEventWithGet(
+        [FromRoute] Guid deviceId,
+        string eventType,
+        string eventData,
+        int? profileId,
+        Db db
+    )
+    {
+        await db.ProcessedEvents.AddAsync(new ProcessedEvent
+        {
+            DeviceId = deviceId,
+            EventType = eventType,
+            EventData = eventData,
+            ProfileId = profileId
         });
 
         await db.SaveChangesAsync();
